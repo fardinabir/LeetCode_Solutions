24b23c261c88d3a7a00885a976f629a1cc43da73
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2022-08-04 12:03:08 +0600

    Time: 111 ms (84.61%), Space: 51.5 MB (51.65%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int i,s;
-        s = nums.size();
         unordered_map <int,bool> ump;
-        for(i=0;i<s;i++){
+        for(int i=0;i<nums.size();i++){
             if(ump.find(nums[i])!=ump.end())
                 return true;
             ump[nums[i]]=true;
