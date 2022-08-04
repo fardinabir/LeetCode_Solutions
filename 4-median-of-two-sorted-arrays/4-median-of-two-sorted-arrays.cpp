045f530c8414309d9f174bdc7c42b0fd40fc96bc
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2022-08-04 21:10:07 +0600

    Time: 45 ms (84.46%), Space: 89.2 MB (86.59%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int i=0,j=0,prev,sz=nums1.size()+nums2.size(),med=sz/2;
-        vector <int> srt;
-        while(i+j<=med && i<nums1.size() && j<nums2.size()){
-            if(nums1[i]<nums2[j]){
-                srt.push_back(nums1[i++]);
-            }
-            else{
-                srt.push_back(nums2[j++]);
-            }
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    if(nums1.size()<nums2.size())
+            return findMedianSortedArrays(nums2,nums1);
+    int s1=nums1.size(),s2=nums2.size(),left1,left2,right1,right2;
+    int lo=0,hi=s2,mid2,mid1;
+    while(lo<=hi){
+        mid2 = (lo+hi)/2;
+        mid1 = (s1+s2+1)/2-mid2;
+        
+        left2 = mid2-1>=0? nums2[mid2-1] : INT_MIN;
+        left1 = mid1-1>=0? nums1[mid1-1] : INT_MIN;
+        right2 = mid2<s2? nums2[mid2] : INT_MAX;
+        right1 = mid1<s1? nums1[mid1] : INT_MAX;
+        
+        if(left2<=right1 && left1<=right2){
+            return (s1+s2)&1? max(left1,left2) : (max(left1,left2)+min(right1,right2))/2.0;
         }
-        while(i>=nums1.size() && i+j<=med){
-            srt.push_back(nums2[j++]);
-        }
-        while(j>=nums2.size() && i+j<=med){
-            srt.push_back(nums1[i++]);
-        }
-        if(sz&1){
-           return srt.back(); 
-        }
-        else{
-            return (srt[srt.size()-1]+srt[srt.size()-2])/2.0;
-        }
+        else if(left1>right2)
+            lo = mid2+1;
+        else
+            hi = mid2-1;
+    }
+    return -1;
     }
 };
