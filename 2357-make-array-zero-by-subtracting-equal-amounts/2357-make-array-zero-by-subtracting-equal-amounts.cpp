0608d60abb3d1bbaffbabd91824eb18b78e70449
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2022-08-30 08:51:40 +0600

    Time: 3 ms (76.46%), Space: 8.3 MB (55.11%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int carry=0,s=0,i=0;
-        for(i=0;i<nums.size();i++){
-            if(nums[i]!=0 && nums[i]>carry){
-                carry+=(nums[i]-carry);
-                s++;
-            }
+        unordered_set <int> ust;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i])
+                ust.insert(nums[i]);
         }
-        return s;
+        return ust.size();
     }
 };
