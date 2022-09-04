c34e89536519c8224361e2a8e4a811144d39d046
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2022-09-04 21:44:24 +0600

    Time: 225 ms (38.47%), Space: 93.2 MB (89.30%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mn=prices[0], mx=0;
+        for(int i=0; i < prices.size(); i++){
+            mx = max(mx, prices[i] - mn);
+            mn = min(mn, prices[i]);
+        }
+        return mx;
+    }
+};
