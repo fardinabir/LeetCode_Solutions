1f095526aa1c180a3836ad12fe0202517b487117
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2022-09-05 15:42:13 +0600

    Time: 43 ms (64.36%), Space: 10.6 MB (69.18%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        vector <int> freq(26,0);
+        int freq[26]={0};
         int indx=-1;
         for(int i=0; i<s.size(); i++){
             freq[s[i]-'a']++;
         }
         for(int i=0; i<s.size(); i++){
-            if(freq[s[i]-'a']==1){
-                indx = i;
-                break;
-            }
+            if(freq[s[i]-'a']==1)
+                return i;
         }
-        return indx;
+        return -1;
     }
 };
