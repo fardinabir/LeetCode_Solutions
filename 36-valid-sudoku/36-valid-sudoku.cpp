a9644a094746c90e97d546403145fcf077c8b2be
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2022-09-05 09:44:46 +0600

    Time: 59 ms (11.98%), Space: 20.9 MB (14.30%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map <int,bool> row[9],col[9],box[9];
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j]!='.'){
+                    int val = board[i][j];
+                    if(row[i][val] || col[j][val] || box[(i/3)*3+j/3][val]){
+                        cout<<(i/3)*3+j/3<<" "<< box[(i/3)*3+j/3][val]<<endl;
+                        cout<<i<<" -- "<<j<<endl;
+                        return false;
+                    }
+                    row[i][val] = col[j][val] = box[(i/3)*3 + j/3][val] = true;
+                }
+            }
+        }
+        return true;
+    }
+};
