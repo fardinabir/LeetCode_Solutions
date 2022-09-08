5ab111770f7ebb3a8d40a3021d7dfb081627c217
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2022-09-08 11:54:50 +0600

    Time: 0 ms (100.00%), Space: 7.1 MB (56.56%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -1,35 +1,29 @@
 class MyQueue {
 public:
     stack <int> main, aux;
+    int front;
     void push(int x) {
+        if(main.empty())
+            front = x;
         main.push(x);
     }
-    int pop() {
-        while(!main.empty()){
-            aux.push(main.top());
-            main.pop();
+    int pop(){
+        if(aux.empty()){
+            while(!main.empty()){
+                aux.push(main.top());
+                main.pop();
+            }
         }
         int top = aux.top();
         aux.pop();
-        while(!aux.empty()){
-            main.push(aux.top());
-            aux.pop();
-        }
         return top;
     }
     int peek(){
-        while(!main.empty()){
-            aux.push(main.top());
-            main.pop();
-        }
-        int top = aux.top();
-        while(!aux.empty()){
-            main.push(aux.top());
-            aux.pop();
-        }
-        return top;
+        if(!aux.empty())
+            return aux.top();
+        return front;
     }
     bool empty() {
-        return main.empty();
+        return main.empty() && aux.empty();
     }
 };
