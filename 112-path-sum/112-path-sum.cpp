af5cc1555849bd35d608616739860923f94087f0
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2022-09-09 21:48:36 +0600

    Time: 21 ms (35.35%), Space: 21.2 MB (92.17%) - LeetHub

---
+++ 112-path-sum/112-path-sum.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(!root || (!root->left && !root->right))
+            return root? targetSum==root->val : false;
+        return hasPathSum(root->left, targetSum - root->val)|hasPathSum(root->right, targetSum - root->val);
+    }
+};
