63b1b8d077de4498227e51042155eb6facebd646
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2022-09-20 10:58:12 +0600

    Time: 647 ms (47.25%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
 SELECT employee_id FROM Employees WHERE employee_id not in (SELECT employee_id FROM Salaries)
 UNION
 SELECT employee_id FROM Salaries WHERE employee_id not in (SELECT employee_id FROM Employees)
