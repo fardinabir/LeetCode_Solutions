9513b6362c8bd7208379a28fb699a93cd2bd2c9d
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2022-09-21 18:57:43 +0600

    Time: 769 ms (21.45%), Space: 0B (100.00%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,4 @@
+SELECT activity_date day, COUNT(DISTINCT user_id) active_users FROM Activity
+WHERE DATEDIFF('2019-07-27', activity_date) < 30 AND activity_date <= '2019-07-27'
+GROUP BY activity_date
+HAVING count(distinct user_id)>0
