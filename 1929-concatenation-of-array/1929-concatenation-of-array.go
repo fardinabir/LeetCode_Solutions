e66dd639a72f081cb4a533597fb185a04fa2a04d
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2023-12-25 01:07:59 +0600

    Time: 6 ms (72.03%), Space: 5.5 MB (48.26%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.go
+++ 1929-concatenation-of-array/1929-concatenation-of-array.go
@@ -1,7 +1,3 @@
 func getConcatenation(nums []int) []int {
-    lenght := len(nums)
-    for i := 0; i < lenght; i++ {
-        nums = append(nums, nums[i%lenght]) 
-    }
-    return nums
+    return append(nums, nums...)
 }
