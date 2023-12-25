02e8b98e0c89ead60407c68783c1d41218082cbe
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2023-12-25 21:16:59 +0600

    Time: 3 ms (15.73%), Space: 2.1 MB (70.56%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.go
+++ 0091-decode-ways/0091-decode-ways.go
@@ -1,34 +1,24 @@
-var mem map[string]int
+var mem []int
 
 func numDecodings(s string) int {
-    mem = make(map[string]int)
+    mem = make([]int, len(s)+1)
     return calDecodings(0, s)
 }
 
 func calDecodings(i int, s string) int {
     if len(s) <= i {
-        // fmt.Println(cons)
         return 1
     }
     if s[i] == '0' {
         return 0
     }
-    value, ok := mem[s[i:]]
-    if ok {
-        return value
+    if mem[i] != 0 {
+        return mem[i]
     }
-    var nextDigit, oneDigit int
-    if s[i] != '0' {
-        // fmt.Println("-----", s[:i+1])
-        oneDigit = calDecodings(i+1, s)
+    oneDigit := calDecodings(i+1, s)
+    if i+1 < len(s) && (s[i] == '1' || (s[i] == '2' && s[i+1] <= '6')) {
+        oneDigit += calDecodings(i+2, s)
     }
-    if i+1 < len(s)  {
-        num, _ := strconv.Atoi(s[i:i+2])
-        if num < 27 && num > 0 {
-        // fmt.Println("-----", cons)
-        nextDigit = calDecodings(i+2, s)
-        }
-    }
-    mem[s[i:]] = oneDigit + nextDigit
-    return oneDigit + nextDigit
+    mem[i] = oneDigit
+    return oneDigit
 }
