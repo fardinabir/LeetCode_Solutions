fe9357dd69457ea051f957bdd6ceba48af866513
Kazi Fardin Islam Abir <fardinabir@gmail.com>
2024-01-11 16:32:30 +0600

    Time: 3 ms (75.00%), Space: 4.1 MB (95.24%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.go
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.go
@@ -1,23 +1,15 @@
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
 func maxAncestorDiff(root *TreeNode) int {
     return findMaxDiff(root, root.Val, root.Val)
 }
 
 func findMaxDiff(node *TreeNode, mx, mn int) int {
     if node == nil {
-        return 0
+        return mx - mn
     }
     mx = max(mx, node.Val)
     mn = min(mn, node.Val)
-    return max(max(mx - node.Val, node.Val - mn), max(findMaxDiff(node.Left, mx, mn),
-                         findMaxDiff(node.Right, mx, mn)))
+    return max(findMaxDiff(node.Left, mx, mn),
+                         findMaxDiff(node.Right, mx, mn))
 }
 
 func max(a, b int) int {
